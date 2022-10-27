ae01fcc0f3c58be0d974221f0976e2f09be5ab51
Juho Kangas <jkangas@student.hive.fi>
2022-10-27 15:22:37 +0300

    finish part1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,34 +1,64 @@
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
-const Button = ({title, handleClick}) => {
-  return (
-    <div>
-      <button onClick={handleClick}>{title}</button>
-    </div>
-  )
-}
+const Button = ({ title, handleClick }) => {
+	return (
+		<div>
+			<button onClick={handleClick}>{title}</button>
+		</div>
+	);
+};
+
+const Title = (props) => <h1>{props.text}</h1>;
+
+const Votes = (props) => <div>has {props.pointsAmount} votes</div>;
+
+const Anecdote = (props) => <p>{props.anecdote}</p>;
 
 const App = () => {
-  const [selected, setSelected] = useState(0)
+	const [selected, setSelected] = useState(0);
+	const [points, setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 });
 
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
+	const anecdotes = [
+		"If it hurts, do it more often.",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+	];
 
-  const randomNum = () => Math.floor(Math.random() * anecdotes.length)
-  
-  return (
-    <div>
-      {anecdotes[selected]}
-      <Button title="next anecdote" handleClick={() => setSelected(randomNum)} />
-    </div>
-  )
-}
+	const updatePoints = (anecdote) => {
+		let newPoints = { ...points };
+		newPoints[anecdote] += 1;
+		setPoints({
+			...newPoints,
+		});
+		console.log(points);
+	};
 
-export default App
+	const randomNum = () => Math.floor(Math.random() * anecdotes.length);
+
+	const getHighestVoted = () => {
+		let pointsArr = Object.values(points);
+		return pointsArr.indexOf(Math.max(...pointsArr));
+	};
+
+	console.log(getHighestVoted());
+
+	return (
+		<div>
+			<Title text="Anecdote of the day" />
+			<Anecdote anecdote={anecdotes[selected]} />
+			<Votes pointsAmount={points[selected]} />
+			<Button title="vote" handleClick={() => updatePoints(selected)} />
+			<Button title="next anecdote" handleClick={() => setSelected(randomNum())} />
+			<Title text="Anecdote with most Votes" />
+			<Anecdote anecdote={anecdotes[getHighestVoted()]} />
+			<Votes pointsAmount={points[getHighestVoted()]} />
+		</div>
+	);
+};
+
+export default App;
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,52 +5,56 @@ const Header = ({ title }) => <h1>{title}</h1>;
 const Button = ({ name, handleClick }) => <button onClick={handleClick}>{name}</button>;
 
 const StatisticsLine = ({ title, value }) => {
-  return (
-    <>
-      <tr>
-        <td>{title}</td>
-        <td>{value}</td>
-      </tr>
-    </>
-  );
+	return (
+		<>
+			<tr>
+				<td>{title}</td>
+				<td>{value}</td>
+			</tr>
+		</>
+	);
 };
 
 const Statistics = ({ good, neutral, bad }) => {
-  const sum = good + neutral + bad;
-  const average = (good - bad) / sum;
-  const positive = (good / sum) * 100 + " %";
+	const sum = good + neutral + bad;
+	const average = (good - bad) / sum;
+	const positive = (good / sum) * 100 + " %";
 
-  if (sum > 0) {
-    return (
-      <>
-        <StatisticsLine title="good" value={good} />
-        <StatisticsLine title="neutral" value={neutral} />
-        <StatisticsLine title="bad" value={bad} />
-        <StatisticsLine title="all" value={sum} />
-        <StatisticsLine title="average" value={average} />
-        <StatisticsLine title="positive" value={positive} />
-      </>
-    );
-  } else {
-    return <p>No feedbacks given</p>;
-  }
+	if (sum > 0) {
+		return (
+			<>
+				<table>
+					<tbody>
+						<StatisticsLine title="good" value={good} />
+						<StatisticsLine title="neutral" value={neutral} />
+						<StatisticsLine title="bad" value={bad} />
+						<StatisticsLine title="all" value={sum} />
+						<StatisticsLine title="average" value={average} />
+						<StatisticsLine title="positive" value={positive} />
+					</tbody>
+				</table>
+			</>
+		);
+	} else {
+		return <p>No feedbacks given</p>;
+	}
 };
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <Header title="give feedback" />
-      <Button handleClick={() => setGood(good + 1)} name="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} name="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} name="bad" />
-      <Header title="statistics" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
-  );
+	return (
+		<div>
+			<Header title="give feedback" />
+			<Button handleClick={() => setGood(good + 1)} name="good" />
+			<Button handleClick={() => setNeutral(neutral + 1)} name="neutral" />
+			<Button handleClick={() => setBad(bad + 1)} name="bad" />
+			<Header title="statistics" />
+			<Statistics good={good} neutral={neutral} bad={bad} />
+		</div>
+	);
 };
 
 export default App;
