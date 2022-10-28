723f372615491fd13d4dead61668b3e6f3dcef21
Juho Kangas <jkangas@student.hive.fi>
2022-10-28 10:14:39 +0300

    finish phonebook step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -10,6 +10,48 @@ const Person = ({ name, number }) => {
 
 const Title = ({ title }) => <h2>{title}</h2>;
 
+const PersonForm = (props) => {
+	return (
+		<form>
+			<div>
+				name:{" "}
+				<input value={props.newName} onChange={props.handleNameInput} />
+			</div>
+			<div>
+				number:{" "}
+				<input
+					value={props.newNumber}
+					onChange={props.handleNumberInput}
+				/>
+			</div>
+			<div>
+				<button onClick={props.handleAdd} type="submit">
+					add
+				</button>
+			</div>
+		</form>
+	);
+};
+
+const Filter = (props) => {
+	return (
+		<div>
+			filter shown with:{" "}
+			<input
+				type="text"
+				value={props.search}
+				onChange={props.handleSearch}
+			/>
+		</div>
+	);
+};
+
+const Persons = ({ data }) => {
+	return data.map((person) => (
+		<Person key={person.id} name={person.name} number={person.number} />
+	));
+};
+
 const App = () => {
 	const [persons, setPersons] = useState([
 		{ name: "Arto Hellas", number: "040-123456", id: 1 },
@@ -20,7 +62,7 @@ const App = () => {
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [search, setSearch] = useState("");
-	const [filteredList, setFilteredList] = useState(persons);
+	const [filteredList, setFilteredList] = useState([]);
 
 	const handleAdd = (e) => {
 		e.preventDefault();
@@ -30,7 +72,11 @@ const App = () => {
 		if (persons.some(checkName)) {
 			alert(`${newName} is already added to the phonebook`);
 		} else {
-			const newPerson = { name: newName, number: newNumber };
+			const newPerson = {
+				name: newName,
+				number: newNumber,
+				id: persons.length + 1,
+			};
 			setPersons(persons.concat(newPerson));
 			setNewName("");
 			setNewNumber("");
@@ -56,33 +102,21 @@ const App = () => {
 	return (
 		<div>
 			<Title title={"Phonebook"} />
-			<div>
-				filter shown with:{" "}
-				<input type="text" value={search} onChange={handleSearch} />
-			</div>
+			<Filter search={search} handleSearch={handleSearch} />
 			<Title title={"add a new"} />
-			<form>
-				<div>
-					name: <input value={newName} onChange={handleNameInput} />
-				</div>
-				<div>
-					number:{" "}
-					<input value={newNumber} onChange={handleNumberInput} />
-				</div>
-				<div>
-					<button onClick={handleAdd} type="submit">
-						add
-					</button>
-				</div>
-			</form>
+			<PersonForm
+				newName={newName}
+				newNumber={newNumber}
+				handleNameInput={handleNameInput}
+				handleNumberInput={handleNumberInput}
+				handleAdd={handleAdd}
+			/>
 			<Title title={"Numbers"} />
-			{filteredList.map((person) => (
-				<Person
-					key={person.id}
-					name={person.name}
-					number={person.number}
-				/>
-			))}
+			{search === "" ? (
+				<Persons data={persons} />
+			) : (
+				<Persons data={filteredList} />
+			)}
 		</div>
 	);
 };
