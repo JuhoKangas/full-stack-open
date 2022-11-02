1ca2d13c183dd4b31fd5fcb71c514d97c24b8775
Juho Kangas <jkangas@student.hive.fi>
2022-11-02 11:33:11 +0200

    finish step 6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
 	{
 		id: 1,
 		name: "Arto Hellas",
@@ -28,6 +28,62 @@ const persons = [
 
 app.get("/api/persons", (req, res) => {
 	res.json(persons);
+});
+
+app.get("/api/info", (req, res) => {
+	const personsLength = persons.length;
+	const timeStamp = new Date();
+	res.send(
+		`<p>Phonebook has info for ${personsLength} people</p><br />${timeStamp}`
+	);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id);
+	const person = persons.find((p) => p.id === id);
+	if (person) {
+		res.json(person);
+	} else {
+		res.status(404).end();
+	}
+});
+
+const generateId = () => {
+	const id = Math.floor(Math.random() * 100000000);
+	return id;
+};
+
+app.post("/api/persons", (req, res) => {
+	const body = req.body;
+
+	if (!body.name || !body.number) {
+		return res.status(400).json({
+			error: "name or number missing",
+		});
+	}
+
+	if (persons.find((p) => p.name === body.name)) {
+		return res.status(400).json({
+			error: "name must be unique",
+		});
+	}
+
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: generateId(),
+	};
+
+	persons = persons.concat(person);
+
+	res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id);
+	persons = persons.filter((p) => p.id !== id);
+
+	res.status(204).end();
 });
 
 const PORT = 3001;
